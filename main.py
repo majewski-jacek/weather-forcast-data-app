96b643edf0d4d2e8aa948a50830c57d39e96710d
DeathMaroko <asikkoko@gmail.com>
2024-02-19 12:14:21 +0100

    Fixing bugs

--- main.py
+++ main.py
@@ -12,21 +12,23 @@ option = st.selectbox("Select data to view",
 st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
-    # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, 
-                        labels={"x": "Date", "y": "Temperatures (C)"})
-        st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, 
+                            labels={"x": "Date", "y": "Temperatures (C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_contitions = [dict["weather"][0]["main"] for dict in filtered_data]
-        imaages_paths = [images[condition] for condition in sky_contitions]
-        st.image(imaages_paths, width=115)
-
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_contitions = [dict["weather"][0]["main"] for dict in filtered_data]
+            imaages_paths = [images[condition] for condition in sky_contitions]
+            st.image(imaages_paths, width=115)
+    except KeyError:
+        st.info("The place doesn't exist")
